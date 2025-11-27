72be1ffc05c7e88a1f022ac7b7d9ee5b6388a1e0
Shine-5705 <guptashine5002@gmail.com>
2025-11-28 04:36:07 +0530

    modified

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -7,7 +7,7 @@ export const useProducts = () => {
   const [error, setError] = useState<string | null>(null)
   
   // This will cause a TypeScript error - assigning number to string
-  const invalidTypeAssignment: string = 1234578
+  const invalidTypeAssignment: string = 12345333333
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
